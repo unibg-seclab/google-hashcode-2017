ae15c05a3b2919db5d64e9912dee5c4177df9bda
enricobacis <enrico.bacis@gmail.com>
2017-02-28 13:46:33 +0100

    use a priority queue with an entry for each video/cache

--- solve.py
+++ solve.py
@@ -1,6 +1,7 @@
 from multiprocessing import Pool
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from bisect import insort
+import heapq
 import sys
 
 class Cache:
@@ -10,11 +11,11 @@ class Cache:
         self.remaining = capacity
         self.videos = set()
         self.endpoints = set()
-        self.utility = 0
+        self.benefits = defaultdict(int)
 
     @property
     def cost(self):
-        return self.utility * (2 - self.remaining / float(self._capacity))
+        return 1
 
 def row(fn):
     return map(fn, raw_input().strip().split())
@@ -41,25 +42,15 @@ raw_requests = set() # just for last iteration
 requests = [[0 for e in xrange(nendpoints)] for v in xrange(nvideos)]
 for r in xrange(type_requests):
     video_id, endpoint_id, numrequests = row(int)
+    if videos[video_id] > cachesize: continue       # skips videos too big to cache
     requests[video_id][endpoint_id] += numrequests
     raw_requests.add((video_id,endpoint_id))
 
     for cache_id, latency_to_cache in endpoints[endpoint_id].iteritems():
         cache = caches[cache_id]
-        cache.utility += max(0, latencies[endpoint_id] - latency_to_cache) * numrequests
-
-# normalize utilities
-min_utility = min(cache.utility for cache in caches)
-max_utility = max(cache.utility for cache in caches)
-delta = float(max_utility - min_utility)
-for cache in caches:
-    if not delta: cache.utility = 1.0
-    else: cache.utility = ((cache.utility - min_utility) / float(delta)) + 1
-    sys.stderr.write('%f\n' % cache.utility)
-
-import time
-time.sleep(1)
-
+        request_benefit = (latencies[endpoint_id] - latency_to_cache) * numrequests
+        if request_benefit > 0:
+            cache.benefits[video_id] += request_benefit
 
 assert len(videos) == nvideos
 assert len(latencies) == nendpoints
@@ -67,36 +58,34 @@ assert len(endpoints) == nendpoints
 
 current_latencies = [[latencies[eid] for eid in xrange(nendpoints)] for vid in xrange(nvideos)]
 
+for cache_id, cache in enumerate(caches):
+    cache.benefits = sorted((b, vid) for vid, b in cache.benefits.iteritems() if b > 0)
+    requested_size = sum(videos[vid] for b, vid in cache.benefits)
+    if requested_size <= cachesize:
+        cache.videos = [vid for vid, b in cache.benefits]
+        cache.remaining = 0
 
-def solve(video_id):
+def solve(video_id, cache_id):
     video_size = videos[video_id]
     videorequests = requests[video_id]
     videolatencies = current_latencies[video_id]
-    best_benefit = 0
-    best_cache = None
 
-    for cache_id, cache in enumerate(caches):
-        if video_size > cache.remaining: continue
-        overall_benefit = 0
+    if video_size > cache.remaining: return 0
+    benefit = 0
 
-        for endpoint_id, endpoint in enumerate(endpoints):
-            if cache_id not in endpoint: continue
-            nrequest = videorequests[endpoint_id]
-            if not nrequest: continue
-            current_latency = videolatencies[endpoint_id]
-            latency = endpoint[cache_id]
+    for endpoint_id, endpoint in enumerate(endpoints):
+        if cache_id not in endpoint: continue
+        nrequest = videorequests[endpoint_id]
+        if not nrequest: continue
+        current_latency = videolatencies[endpoint_id]
+        latency = endpoint[cache_id]
 
-            if latency < current_latency:
-                # punto da tarare
-                latency_benefit = (current_latency - latency) * nrequest
-                overall_benefit += latency_benefit
+        if latency < current_latency:
+            # punto da tarare
+            benefit += (current_latency - latency) * nrequest
 
-        overall_benefit_density = overall_benefit / (float(video_size) * caches[cache_id].cost)
-        if overall_benefit_density > best_benefit:
-            best_benefit = overall_benefit_density
-            best_cache = cache_id
-
-    return best_benefit, best_cache
+    #overall_benefit_density = benefit / (float(video_size) * caches[cache_id].cost)
+    return benefit
 
 
 for video_id, video_size in enumerate(videos):
@@ -104,31 +93,34 @@ for video_id, video_size in enumerate(videos):
     for endpoint_id in xrange(nendpoints):
         reqs += requests[video_id][endpoint_id]
 
-video_benefits = Pool(processes=8).map(solve, range(nvideos))
-sorted_videos = [(b[0], video_id) for video_id, b in enumerate(video_benefits)]
+#video_benefits = Pool(processes=8).map(solve, range(nvideos))
+#sorted_videos = [(b[0], video_id) for video_id, b in enumerate(video_benefits)]
+
+sorted_videos = [(b, vid, cid) for cid, cache in enumerate(caches) for b, vid in cache.benefits]
 sorted_videos.sort()   # remove from last!
 
 try:
     while sorted_videos:
-        _, video_id = sorted_videos.pop()
+        _, video_id, cache_id = sorted_videos.pop()
         video_size = videos[video_id]
 
-        while True:
-            benefit, cache_id = solve(video_id)
-            if benefit <= 0: break
-            if len(sorted_videos) > 1 and sorted_videos[-2][0] > benefit:
-                insort(sorted_videos, (benefit, video_id))
-                break
+        benefit = solve(video_id, cache_id)
+        if benefit <= 0: continue
+        print cid, cache_id
+        if len(sorted_videos) > 1 and sorted_videos[-2][0] > benefit:
+            sys.stderr.write('relocate\n')
+            insort(sorted_videos, (benefit, video_id, cache_id))
+            continue
 
-            sys.stderr.write('length %d benefit %d\n' % (len(sorted_videos), benefit))
-            cache = caches[cache_id]
-            cache.videos.add(video_id)
-            cache.remaining -= video_size
+        sys.stderr.write('length %d benefit %d\n' % (len(sorted_videos), benefit))
+        cache = caches[cache_id]
+        cache.videos.add(video_id)
+        cache.remaining -= video_size
 
-            for endpoint_id in caches[cache_id].endpoints:
-                current_latencies[video_id][endpoint_id] = \
-                        min(current_latencies[video_id][endpoint_id],
-                            endpoints[endpoint_id][cache_id])
+        for endpoint_id in caches[cache_id].endpoints:
+            current_latencies[video_id][endpoint_id] = \
+                    min(current_latencies[video_id][endpoint_id],
+                        endpoints[endpoint_id][cache_id])
 
 except KeyboardInterrupt: pass
 
