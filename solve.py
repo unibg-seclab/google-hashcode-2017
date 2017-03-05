e36ab6bedb731bfd0cb44954e2f90c52dac2717d
enricobacis <enrico.bacis@gmail.com>
2017-03-05 21:25:03 +0100

    use precomputed latency values

--- solve.py
+++ solve.py
@@ -1,13 +1,15 @@
 from multiprocessing import Pool
 from collections import defaultdict
 from operator import itemgetter
-from bisect import insort
+from bisect import bisect_left
+import random
+import math
 import sys
 import os
 
-import random
-seed = int(os.urandom(16).encode('hex'), 16)
-random.seed(seed)
+#seed = int(os.urandom(16).encode('hex'), 16)
+#random.seed(seed)
+random.seed(0)
 
 class Cache:
     def __init__(self):
@@ -60,7 +62,7 @@ for endpoint_id in xrange(nendpoints):
     endpoints.append(endpoint_latencies)
 
 totrequests = 0
-requests = [[0 for e in xrange(nendpoints)] for v in xrange(nvideos)]
+requests = defaultdict(lambda: defaultdict(int))
 for r in xrange(type_requests):
     video_id, endpoint_id, numrequests = row(int)
     requests[video_id][endpoint_id] += numrequests
@@ -73,6 +75,26 @@ assert len(videosize) == nvideos
 assert len(latencies) == nendpoints
 assert len(endpoints) == nendpoints
 
+_current_latencies = [[[] for eid in xrange(nendpoints)] for vid in xrange(nvideos)]
+
+def get_current_latency(video_id, endpoint_id, excluding_cache=None):
+    latency_pairs = _current_latencies[video_id][endpoint_id]
+    idx = 0
+    while idx < len(latency_pairs):
+        latency, cache_id = latency_pairs[idx]
+        if cache_id == excluding_cache: idx += 1
+        elif video_id not in caches[cache_id].videos: latency_pairs.pop(idx)
+        else: return latency
+    return latencies[endpoint_id]
+
+def update_current_latency(video_id, endpoint_id, cache_id):
+    latency_pairs = _current_latencies[video_id][endpoint_id]
+    latency = endpoints[endpoint_id][cache_id]
+    latency_pair = (latency, cache_id)
+    idx = bisect_left(latency_pairs, latency_pair)
+    if idx == len(latency_pairs) or latency_pairs[idx] != latency_pair:
+        latency_pairs.insert(idx, latency_pair)
+
 def compute_video_values(cache_id):
     cache = caches[cache_id]
     values = defaultdict(int)
@@ -84,30 +106,34 @@ def compute_video_values(cache_id):
             numrequests = videorequests[endpoint_id]
             if not numrequests: continue
             endpoint = endpoints[endpoint_id]
-            current_latency = latencies[endpoint_id]
-
-            for connected_cache_id, connected_cache_latency in endpoint.iteritems():
-                if cache_id == connected_cache_id: continue
-                if video_id in caches[connected_cache_id].videos:
-                    if connected_cache_latency < current_latency:
-                        current_latency = connected_cache_latency
-
+            current_latency = get_current_latency(video_id, endpoint_id, excluding_cache=cache_id)
             if endpoint[cache_id] < current_latency:
                 values[video_id] += numrequests * (current_latency - endpoint[cache_id])
 
     return values
 
+def compute_score():
+    score = 0
+    for video_id in xrange(nvideos):
+        for endpoint_id, numrequests in requests[video_id].iteritems():
+            latency = latencies[endpoint_id]
+            current_latency = min(latency, get_current_latency(video_id, endpoint_id))
+            score += (latency - current_latency) * numrequests
+    return int(math.floor(1000.0 * score / totrequests))
+
+def make_output():
+    print sum(1 for cache in caches if cache.videos)
+    for cache_id, cache in enumerate(caches):
+        if cache.videos: print cache_id, ' '.join(map(str, cache.videos))
+
 try:
     for iteration in xrange(100):
-        sys.stderr.write('iteration %d\n' % iteration)
-
         cache_ids = range(ncaches)
         random.shuffle(cache_ids)
 
         for cache_id in cache_ids:
             cache = caches[cache_id]
 
-            sys.stderr.write('cache %d\n' % cache_id)
             values = compute_video_values(cache_id)
             items = [(video_id, videosize[video_id], values[video_id])
                     for video_id in values.iterkeys()]
@@ -115,15 +141,13 @@ try:
             items_to_cache, benefit = knapsack(items, cachesize)
             videos_to_cache = set(map(itemgetter(0), items_to_cache))
             cache.videos = videos_to_cache
-            sys.stderr.write('benefit: %s\n' % (1000.0 * benefit / totrequests))
+            sys.stderr.write('cache: %d benefit: %d\n' % (cache_id, 1000.0 * benefit / totrequests))
+
+            for video_id in cache.videos:
+                for endpoint_id in cache.endpoints:
+                    update_current_latency(video_id, endpoint_id, cache_id)
+
+        sys.stderr.write('iteration %d score: %d\n' % (iteration, compute_score()))
+
 except KeyboardInterrupt: pass
-
-
-used_caches = sum(1 for cache in caches if cache.videos)
-print used_caches
-
-for cache_id, cache in enumerate(caches):
-    if cache.videos:
-        print cache_id, ' '.join(map(str, cache.videos))
-
-sys.stderr.write('%s\n' % seed)
+finally: make_output()
